588edf0f3cc816289cbc927d32c07995fe59dae8
Moustafa Ezz <65693055+mooezz999@users.noreply.github.com>
2021-06-06 21:38:39 +0200

    Components and Props

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+const Header = (props) => {
+    return (
+        <header>
+          <h1>{props.title}</h1>  
+        </header>
+    )
+}
+
+Header.defaultProps ={
+    title: 'balah'
+}
+Header.propTypes={
+    title: PropTypes.string,
+}
+export default Header
